0c0fdeb72d5bd4fcba961613515064a796446bce
three4c <three4c@gmail.com>
2021-04-23 09:57:51 +0900

    expandのテストを追加

--- src/components/Song/Song.test.tsx
+++ src/components/Song/Song.test.tsx
@@ -31,8 +31,57 @@ describe('<Song />', () => {
     },
   ];
 
+  const expandItems = [
+    {
+      animeName: 'プラスティック・メモリーズ',
+      animeClassification: 'TV',
+      musicName: 'Ring of Fortune',
+      artistName: '佐々木恵梨',
+      animeDescription: 'OP',
+      broadcastOrder: '',
+      broadcastStartDate: '2015-04-05',
+    },
+    {
+      animeName: 'プラスティック・メモリーズ',
+      animeClassification: 'TV',
+      musicName: 'again & again',
+      artistName: 'メロディー・チューバック',
+      animeDescription: 'ED',
+      broadcastOrder: '第1話のみ放映',
+      broadcastStartDate: '2015-04-05',
+    },
+    {
+      animeName: 'プラスティック・メモリーズ',
+      animeClassification: 'TV',
+      musicName: '朝焼けのスターマイン',
+      artistName: '今井麻美',
+      animeDescription: 'ED',
+      broadcastOrder: '1',
+      broadcastStartDate: '2015-04-05',
+    },
+    {
+      animeName: 'プラスティック・メモリーズ',
+      animeClassification: 'TV',
+      musicName: 'Ring of Fortune',
+      artistName: '佐々木恵梨',
+      animeDescription: 'ED',
+      broadcastOrder: 'スポット放映',
+      broadcastStartDate: '2015-04-05',
+    },
+  ];
+
   it('SnapShot（works）', () => {
     expect(shallow(<Song items={items} />)).toMatchSnapshot();
+  });
+
+  it('SnapShot（works） - expand（isOpen: false）', () => {
+    expect(shallow(<Song items={expandItems} />)).toMatchSnapshot();
+  });
+
+  it('SnapShot（works） - expand（isOpen: true）', () => {
+    const wrapper = mount(<Song items={expandItems} />);
+    wrapper.find('.Song__button').simulate('click');
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('useEffect Test', () => {
